2e98d0ba3755004e2dcf780052132ac49f0f6b43
badoco2 <alexandre.badoco@hotmail.com>
2021-06-04 09:06:29 -0300

    2
    Class peca , cor, tabuleiro

---
+++ JogoXadrez/Tabuleiro/Cor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JogoXadrez.Tabuleiro
+{
+    class Cor
+    {
+    }
+}
---
+++ JogoXadrez/Tabuleiro/Peca.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qtdMovimento { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+    }
+}
