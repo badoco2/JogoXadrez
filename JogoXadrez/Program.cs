c3900510d74a4be18c1666ca0bbb60a0ddbe14d1
badoco2 <alexandre.badoco@hotmail.com>
2021-06-04 10:44:37 -0300

    5
    Tratando erros com exeception

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -8,11 +8,20 @@ namespace JogoXadrez
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-            Tela.imprimeTabuleiro(tab);
+
+
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 9));
+                Tela.imprimeTabuleiro(tab);
+            }
+            catch (TabuleiroExeception e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             Console.ReadLine();
 
