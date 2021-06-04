6178a94f38e12052f2ba7ac8173636d96a38f844
badoco2 <alexandre.badoco@hotmail.com>
2021-06-04 12:27:21 -0300

    6
    Finalizado

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -2,29 +2,50 @@
 using tabuleiro;
 using xadrez;
 
-namespace JogoXadrez
+namespace jogoXadrez
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+    class Program {
+        static void Main(string[] args) {
 
+            try {
+                PartidaDeXadrez partida = new PartidaDeXadrez();
 
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 9));
-                Tela.imprimeTabuleiro(tab);
+                while (!partida.terminada) {
+
+                    try {
+                        Console.Clear();
+                        Tela.imprimirPartida(partida);
+
+                        Console.WriteLine();
+                        Console.Write("Origem: ");
+                        Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeOrigem(origem);
+
+                        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                        Console.WriteLine();
+                        Console.Write("Destino: ");
+                        Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeDestino(origem, destino);
+
+                        partida.realizaJogada(origem, destino);
+                    }
+                    catch (TabuleiroException e) {
+                        Console.WriteLine(e.Message);
+                        Console.ReadLine();
+                    }
+                }
+                Console.Clear();
+                Tela.imprimirPartida(partida);
             }
-            catch (TabuleiroExeception e)
-            {
+            catch (TabuleiroException e) {
                 Console.WriteLine(e.Message);
             }
 
             Console.ReadLine();
-
         }
     }
 }
--- JogoXadrez/Tabuleiro/Peca.cs
+++ JogoXadrez/Tabuleiro/Peca.cs
@@ -1,25 +1,42 @@
-﻿
-namespace tabuleiro
-{
-    abstract class Peca
-    {
+﻿namespace tabuleiro {
+    abstract class Peca {
+
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
-        public int qtdMovimento { get; protected set; }
+        public int qteMovimentos { get; protected set; }
         public Tabuleiro tab { get; protected set; }
-
-        public Peca(Cor cor, Tabuleiro tab)
-        {
+         
+        public Peca(Tabuleiro tab, Cor cor) {
             this.posicao = null;
-            this.cor = cor;
-            this.qtdMovimento = 0;
             this.tab = tab;
+            this.cor = cor;
+            this.qteMovimentos = 0;
         }
 
-        protected Peca(Tabuleiro tab, Cor cor)
-        {
-            this.tab = tab;
-            this.cor = cor;
+        public void incrementarQteMovimentos() {
+            qteMovimentos++;
         }
+
+        public void decrementarQteMovimentos() {
+            qteMovimentos--;
+        }
+
+        public bool existeMovimentosPossiveis() {
+            bool[,] mat = movimentosPossiveis();
+            for (int i=0; i<tab.linhas; i++) {
+                for (int j=0; j<tab.colunas; j++) {
+                    if (mat[i, j]) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool movimentoPossivel(Posicao pos) {
+            return movimentosPossiveis()[pos.linha, pos.coluna];
+        }
+
+        public abstract bool[,] movimentosPossiveis();
     }
 }
